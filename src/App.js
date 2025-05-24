4f3562c6a028ea0cd90ee6b334a218ed018d0d20
sosaker <j56578510@gmail.com>
2025-05-24 19:18:15 +0300

    Твой комментарий

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import { useEffect } from 'react';
 import './App.css';
-import { useTelegram } from './hooks/useTelegram';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
@@ -16,11 +19,14 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <Header/>
-        <button onClick={onToggleButton}>toggle</button>
-    </div>
-  );
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
